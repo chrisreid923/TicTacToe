6137276bc3ae251b968fc4c8e2b513229b46230c
chrisreid923 <americanman23@gmail.com>
2018-11-27 12:05:09 -0600

    TicTacToe Update 1

---
+++ src/main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class main {
+	    public static void main(String[] args) {
+	        
+	    	Scanner scan = new Scanner(System.in);
+	    	
+//The playing field will be these integers. 	    	
+	    	int UL = 0;
+	    	int L = 0;
+	    	int LL = 0;
+	    	
+	    	int UC = 0;
+	    	int C = 0;
+	    	int LC = 0;
+	    	
+	    	int UR = 0;
+	    	int R = 0;
+	    	int LR = 0;
+	    	
+	    	System.out.println("Pick a Space on the Board: UpperLeft, L, LL, UC, C, LC, UR, R, LR");
+	    	String UpperLeft = null;
+			if (scan.nextLine() == UpperLeft) {
+	    	
+	    	}
+	}
+}
